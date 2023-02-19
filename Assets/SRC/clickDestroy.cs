f3eaf2fd23ab7e7c85686210daed41aaed9cbc0a
Josh Mlaker <joshua.mlaker@marquette.edu>
2023-02-19 13:43:15 -0600

    Updated code to add some comments; Created first builds

--- Assets/SRC/clickDestroy.cs
+++ Assets/SRC/clickDestroy.cs
@@ -14,11 +14,16 @@ public class clickDestroy : MonoBehaviour
         scoreTextMesh = GameObject.Find("Score").GetComponent<TMP_Text>();
     }
 
-    // Called once per click
+    // Called once per click on gameObject
     void OnMouseDown()
     {
+        // Parse current score from score text
         var curScore = int.Parse(scoreTextMesh.text.Split(' ')[scoreTextMesh.text.Split(' ').Length - 1]);
+        // Update score to current score + 1
         scoreTextMesh.text = scoreTextMesh.text.Split(' ')[0] + " " + (curScore + 1);
+        // Update position of game object to random position in the sqaure [0, 10] X [0, 10]
         this.gameObject.transform.position = new Vector3(Random.Range(0, 10), Random.Range(0, 10), 0);
+        // Output "Clicked!"
+        Debug.Log("Clicked!");
     }
 }
